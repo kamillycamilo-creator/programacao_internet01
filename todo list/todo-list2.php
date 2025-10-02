b2894eabbea76b6babd56f1b16ec74e426edcffd
kamillycamilo-creator <kamilly.camilo@escolar.ifrn.edu.br>
2025-10-02 11:45:01 -0300

    Adicao da funcao excluir

--- todo list/todo-list2.php
+++ todo list/todo-list2.php
@@ -22,7 +22,15 @@ if(isset($_POST['descricao']) && !empty(trim(string: $_POST['descricao']))){
 }
 # Exclusão de tarefas
 
+if(isset($_GET['delete'])){
+    $id = intval(  $_GET ['delete'] );
 
+    $sqlDelete = "DELETE FROM tarefas WHERE id = $id";
+
+    if($conn->query($sqlDelete) === TRUE){
+        header("location: todo-list2.php");
+}
+}
 # Listar tarefas
 $tarefas=[]; 
 $sqlSelect = "SELECT * from tarefas ORDER BY data_criacao DESC";
@@ -53,7 +61,14 @@ if($result -> num_rows > 0){
 
     <h2>Suas tarefas</h2>
     <?php if(!empty($tarefas)):?>
-    <h3>Suas tarefas</h3>
+    <ul>
+        <?php foreach($tarefas as $tarefa):?>
+            <li>
+                <?php echo $tarefa['descricao']?>
+                <a href="todo-list2.php?delete=<?php echo $tarefa['id']?>">Excluir</a>
+            </li>
+        <?php  endforeach ?>
+    </ul>
     <?php else:?>
     <h3>Não tem tarefas</h3>
     <?php endif;?>
