ccd66cd1ce2508c03bc5b0323784de8869c2dec3
kamillycamilo-creator <kamilly.camilo@escolar.ifrn.edu.br>
2025-10-21 08:01:54 -0300

    upando o codigo certo

--- todolist-mvc/app/controllers/tarefaController.php
+++ todolist-mvc/app/controllers/tarefaController.php
@@ -1,33 +1,30 @@
-<?php 
+<?php
 require_once __DIR__ . '/../models/tarefa.php';
+class TarefaController {
+    private $tarefaModel; 
 
-class TarefaController{
-    private $tarefaModel;
-
-    public function __construct(){  
-        $this->tarefaModel = new Tarefa();
-
-}
-
-## criar
-
-public function criar() {
-    if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
-
-
-     $this->tarefaModel->criar( $_POST['descricao'] );
+    public function __construct(){
+        $this->tarefaModel = new Tarefa(); 
     }
 
-    header( "location: index.php");
-
-}
-
-public function excluir() {
-    if (isset($_GET['delete'])) {
-        $this->tarefaModel->excluir($_GET['delete']);
+    public function index(){
+        $tarefas = $this->tarefaModel->listar(); 
+        include __DIR__ . '/../views/listar.php'; 
     }
-    header( "Location: index.php");
 
+    public function criar(){
+        if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
+            $this->tarefaModel->criar($_POST['descricao']); 
+        }
+        header("Location: index.php"); 
+    }
+
+    public function excluir(){
+        if(isset($_GET['delete'])){
+            $this->tarefaModel->excluir($_GET['delete']); 
+        }
+        header("Location: index.php"); 
+    }
 }
-}
+
 ?>
